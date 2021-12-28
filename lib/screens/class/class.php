69a123b9ec7aadd5330dea94413c3b9ba3bb56de
KaueSena01 <86299739+KaueSena01@users.noreply.github.com>
2021-12-28 08:32:13 -0300

    card class

--- lib/screens/class/class.php
+++ lib/screens/class/class.php
@@ -5,12 +5,39 @@ include '../menu/menu.php';
 <head>
     <title>Cards Demo - Student Location System</title>
 
+    <link rel="stylesheet" href="../components/style_menu.css">
     <link rel="stylesheet" type="text/css" href="../components/style_class.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@300&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
 </head>
+
+<style type="text/css">
+    .tooltip{
+        border-radius: 50%; 
+        margin-right: 6px;
+    }
+    .item-action:hover{
+        height: 100%;
+        width: 100%;
+    }
+    .floatingButton h1 i{
+        padding:  0;
+    }
+    .floatingButton{
+        position: fixed;
+        top: 82.5%;
+        left: 77.5%;
+        background-color: #451d5c;
+        padding: 8px 30px 8px 10px;
+        border-radius: 30px;
+        box-shadow: 2px 3px 12px #232323;
+    }
+    .floatingButton a{
+        color:  white;
+    }
+</style>
 
 <body>
 
@@ -29,35 +56,49 @@ include '../menu/menu.php';
                     while ($row = $sql->fetch(PDO::FETCH_ASSOC)) {
                         echo "
             <a href='students_table.php?id_classRoom=" . $row['id'] . "'>
-                    <div class='sala'>
-            <div class='card bg-c-blue order-card' style='background-image: linear-gradient(" . $row['colorHex'] . ", rgb(60,60,60));'>
-                <div class='card-block'>
-                    <h1 class='m-b-20 title' title='" . $row['class'] . "'>" . $row['class'] . "</h1>
-                    <h2 class='text-right f-right'>
-                    <a class='button-card' href='../forms/student_form.php?id_classRoom=" . $row['id'] . "'>
-                    <div class='tooltip'><i class='fas fa-user-plus'></i>
+            <div class='sala'>
+            
+            <div class='card bg-c-blue order-card' style='background-color: white; box-shadow: 0px 1px 3px #232323;'>
+                
+                <div class='card-room' style='padding: 5px 0 15px 0;'>
+                    <div style='display: flex; justify-content: space-between;' >
+                    <div style='display: block;'>
+                    <h1 style='padding: 0 20px; font-size: 40px; color: grey;'  title='" . $row['class'] . "'>" . $row['class'] . "</h1>
+                    <p style='padding: 0 20px; font-size: 18px; font-weight: normal; color: #999;'  title='" . $row['class'] . "'>50 alunos</p>
+                    </div>
+                    <div >
+                        <h1 style='padding: 0px 20px; font-size: 60px; color: grey;'  title='" . $row['class'] . "'>" . $row['chairWidth'] . "x" . $row['chairLength'] . "</h1>
+                    </div>
+                    </div>
+
+                    <div style='padding: 30px 20px 0 20px;'>
+                    <a href='../forms/student_form.php?id_classRoom=" . $row['id'] . "'>
+                    <div class='tooltip' style='padding: 8px 8px; border: 2px solid #0c8f8f;'>
+                        <div class='item-action'><i class='fas fa-user-plus' style='color: #0c8f8f; font-size: 20px;'></i></div>
                         <span class='tooltiptext'>Cadastrar Aluno</span>
                     </div>
-                </a>
+                    </a>
                     <a class='button-card' href='../map/map_screen.php?id_classRoom=".$row['id']."'>
-                    <div class='tooltip'><i class='fas fa-map'></i>
-                    <span class='tooltiptext'>Gerar Mapeamento</span>
+                    <div class='tooltip' style='padding: 7px 7px; border: 2px solid #a3e3b1; font-size: 15px;'>
+                        <div class='item-action'><i class='fas fa-map' style='color: #a3e3b1;'></i></div>
+                        <span class='tooltiptext'>Mapeamento</span>
                     </div>
                     </a>
-                        <a class='button-card' href='../forms/classRoom_update_form.php?id_classRoom=" . $row['id'] . "'>
-                            <div class='tooltip'><i class='fas fa-pen'></i>
-                                <span class='tooltiptext'>Editar</span>
-                            </div>
-                        </a>
-                        <a class='button-card' href='../../models/classRoom_models.php?id_classRoom=" . $row['id'] . "'>
-                            <div class='tooltip'><i class='fas fa-trash'></i>
-                                <span class='tooltiptext'>Excluir</span>
-                            </div>
-                        </a>
-                        
-                    </h2>
-                    <p class='m-b-0' title='Sala: " . $row['chairWidth'] . "x" . $row['chairLength'] . "'>Sala: " . $row['chairWidth'] . "x" . $row['chairLength'] . "<span class='f-right'></span></p>
-                </div>
+                    <a class='button-card' href='../forms/classRoom_update_form.php?id_classRoom=" . $row['id'] . "'>
+                    <div class='tooltip' style='padding: 7px 7px; border: 2px solid #ffc018; font-size: 15px;'>
+                        <div class='item-action'><i class='fas fa-pen' style='color: #ffc018;'></i></div>
+                        <span class='tooltiptext'>Editar</span>
+                    </div>
+                    </a>
+                    <ahref='../../models/classRoom_models.php?id_classRoom=" . $row['id'] . "'>
+                    <div class='tooltip' style='padding: 6px 9px; border: 2px solid #f60c49; font-size: 15px;'>
+                        <div class='item-action'><i class='fas fa-trash' style='color: #f60c49; '></i></div>
+                        <span class='tooltiptext'>Excluir</span>
+                    </div>
+                    
+                    </a>
+                    </div>
+            </div>
             </div>
         </div> 
         </a>";
@@ -67,26 +108,14 @@ include '../menu/menu.php';
                 }
                 ?>
 
-                <div class="col-md-4 col-xl-3">
-                    <div class="card bg-c-blue order-card" style="background: rgb(200,200,200);">
-                        <div class="card-block">
-                            <a class="button-card" href='../forms/classRoom_form.php'>
-                                <center>
-                                    <div class="tooltip">
-                                        <h1 class="m-b-20"><i class="fas fa-plus" style="font-size: 62pt;"></i></h1>
-
-                                        <span class="tooltiptext">Adicionar sala</span>
-                                    </div>
-                                </center>
-                            </a>
-
-
-                        </div>
-                    </div>
-                </div>
-
             </div>
         </div>
-
+        <div class="floatingButton">
+                <a href='../forms/classRoom_form.php'>
+                <h1 class="m-b-20"><i class="fas fa-plus" style="font-size: 20pt;"></i> Nova sala
+                </h1>
+            </a>
+            
+        </div>
     </div>
 </body>
